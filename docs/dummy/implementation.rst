1e68c82047c1e6d75072971dcec541bc7b25a21f
H. Mo <h.mo@erasmusmc.nl>
2024-12-05 17:42:34 +0100

    central

--- docs/dummy/implementation.rst
+++ docs/dummy/implementation.rst
@@ -11,14 +11,24 @@ The implementation of our federated learning on Vantage6 comprises the following
 - networks.py: Python script for creating CVD risk prediction models which is a neural network (based on the python implementation of DeepSurv)
 
 
-
-
 Central (``central``)
 -----------------
 The central part is responsible for the orchestration and aggregation of the algorithm.
+The central part of the algorithm is defined in the function "central_ci". This function contains the following input variables:
+    ```
+    predictor_cols: a python list of selected predictors from the harmonized FHIR data (i.e., column names for predictors presented in the flattened FHIR data)
+    outcome_cols: a python list of outcome variables from the harmonized FHIR data (i.e., column names for outcome variables presented in the flattened FHIR data)
+    dl_config: hyperparameter values regarding a neural network architecture and its training (specified in the configuration ini file)
+    num_update_iter: the number of aggregation iterations (set to 20 in our PoC)
+    n_fold: the number of folds in the data splitting in each client for train/valid/test(set to 10 in our PoC)
+    (optional) fold_index: the index of the fold that will be used for test (it is for the corrected resampled t-test in the performance evaluation)
+    ```
 
-.. Describe the central function here.
-
+First it gets all organizations (ids) within the collaboration so you can send a task to them.
+Then, it starts the iterations of broadcasting weights/local weight updating/sending back/aggregation, by defining input parameters for a subtask (will be performed by the partial function below) and creating a subtask for all organizations in the collaboration.
+After creating the subtask, it waits until obtain the results (the local model weight in our case) from all the client nodes. 
+Finally, it computes an weighted average of the collected model weights; this weighted average is based on the trained weight of each local model and the number of training samples in each client node. 
+The aggregated weight is then broadcasted to all the client at the beginning of the next iteration.
 
 
 
--- dummy/central_ci.py
+++ dummy/central_ci.py
@@ -17,6 +17,7 @@ from vantage6.algorithm.tools.util import info, warn, error
 from vantage6.algorithm.tools.decorators import algorithm_client
 from vantage6.algorithm.client import AlgorithmClient
 
+## Set random seed for reproducibility and the same initialization
 torch.manual_seed(0)
 torch.use_deterministic_algorithms(True)
 random.seed(0)
@@ -27,27 +28,37 @@ np.random.seed(0)
 def central_ci(
     client: AlgorithmClient, predictor_cols, outcome_cols, dl_config, num_update_iter, n_fold, fold_index
 ) -> Any:
+    '''
+    AlgorithmClient:
+    predictor_cols: a python list of selected predictors from the harmonized FHIR data (i.e., column names for predictors presented in the flattened FHIR data)
+    outcome_cols: a python list of outcome variables from the harmonized FHIR data (i.e., column names for outcome variables presented in the flattened FHIR data)
+    dl_config: hyperparameter values regarding a neural network architecture and its training (specified in the configuration ini file)
+    num_update_iter: the number of aggregation iterations (set to 20 in our PoC)
+    n_fold: the number of folds in the data splitting in each client for train/valid/test(set to 10 in our PoC)
+    (optional) fold_index: the index of the fold that will be used for test (it is for the corrected resampled t-test in the performance evaluation)
+
+    '''
 
     """ Central part of the algorithm """
-    # TODO implement this function. Below is an example of a simple but typical
     # central function.
-
-    # get all organizations (ids) within the collaboration so you can send a
-    # task to them.
+    # get all organizations (ids) within the collaboration so you can send a task to them.
     organizations = client.organization.list()
     org_ids = [organization.get("id") for organization in organizations]
 
-    global_ci_list = []
-    local_ci_list = []
+    global_ci_list = [] # List of C-statistic for performance evaluation
+    local_ci_list = [] # List of C-statistic for performance evaluation
 
-
+    ## Iterations of broadcasting weights/local weight updating/sending back/aggregation
     for i in range(num_update_iter):
-        print ("update iteration: ", i)
-
+        '''
+        "avged_params" is an aggregated weight
+        '''
         if i == 0:
-            avged_params = None
+            # at the first iteration, the aggregated weight should be None
+            avged_params = None 
         else:
-            avged_params = dict2json(avged_params)
+            # from the second iteration, it contained the weighted average of the model weight, it should be json serialized to be broadcasted to the clients again
+            avged_params = dict2json(avged_params) 
 
         # Define input parameters for a subtask
         info("Defining input parameters")
@@ -61,12 +72,10 @@ def central_ci(
                 "update_iter": i,
                 "n_fold": n_fold,
                 "fold_index": fold_index
-
             }
         }
 
-
-        # create a subtask for all organizations in the collaboration.
+        # Create a subtask for all organizations in the collaboration.
         info("Creating subtask for all organizations in the collaboration")
         task = client.task.create(
             input_=input_,
@@ -79,26 +88,20 @@ def central_ci(
         # wait for node to return results of the subtask.
         info("Waiting for results")
         results = client.wait_for_results(task_id=task.get("id"))
-
         info("Results obtained!")
 
+        params_list =[] # params here indicate network parameters; here, model weight
+        num_train_samples_list = [] # the number of training samples for weighted averaging
+        test_risk_pred_list = [] # risk prediction results on test data 
+        test_y_list = [] # ground truth of followup length (label) of test data (only for evaluation)
+        test_e_list = [] # ground truth of event occurence (label) of test data (only for evaluation)
+        test_ci_list = [] # C-statistic based on local model performance evaluation results
 
-        params_list =[]
-        num_train_samples_list = []
-
-        test_risk_pred_list = []
-        test_y_list = []
-        test_e_list = []
-
-        test_ci_list = []
-
-
+        # process the serialized json data from client (returned after completing the subtask by client based on the partial function)
         for r in results:
             params = r["params"]
             num_train_samples = r["num_train_samples"]
             params = json.loads(params) 
-
-
 
             test_cm = r["test_cm"]
             # test_cm = json2dict(test_cm)
@@ -115,29 +118,25 @@ def central_ci(
 
             test_risk_pred_list.append(test_cm['risk_pred'])
             test_y_list.append(test_cm['y'])
-            test_e_list.append(test_cm['e'])
- 
+            test_e_list.append(test_cm['e']) 
             test_ci_list.append(test_eval['ci'])
-
-
             params_list.append(params)
             num_train_samples_list.append(num_train_samples)
 
-        avged_params = fed_avg(params_list, num_train_samples_list)
-        # print ("avged_params", avged_params)
-        
 
-        ## Compute global results
+        ## Weighed averging of the weight from local models
+        # See fed_avg function in utils.py
+        avged_params = fed_avg(params_list, num_train_samples_list)
+        
+        ## All the lines below are merely for performance evaluation
+        ## Collect all the prediction results 
         risk_pred_stack = np.concatenate( test_risk_pred_list, axis=0 )
+
         y_stack = np.concatenate( test_y_list, axis=0 )
         e_stack = np.concatenate( test_e_list, axis=0 )
 
-
         glo_ci = c_index(-risk_pred_stack, y_stack, e_stack)
-
-
         global_ci_list.append(glo_ci)
-
         local_ci_list.append(test_ci_list)
 
         ## Plot training curves
@@ -147,34 +146,16 @@ def central_ci(
             os.makedirs(figure_result_dir)
 
         plot_global_results(global_ci_list, figure_result_dir, "C-statistic")
-
-
         plot_local_results(local_ci_list, figure_result_dir, "C-statistic")
 
-
-        print ("global_ci_list", global_ci_list)
-        print ("local_ci_list", local_ci_list)
-
-
+    # Create a folder for saving results for the corrected resampled t-test
     ttest_dir = os.path.join(current_dir, "ttest_ci")
     if not os.path.exists(ttest_dir):
         os.makedirs(ttest_dir)
 
+    # Save C-statistic results for for the corrected resampled t-test
     np.save(os.path.join(ttest_dir, "global_ci_%s.npy" %fold_index ), np.array(global_ci_list))
     np.save(os.path.join(ttest_dir, "local_ci_%s.npy" %fold_index), np.array(local_ci_list))
 
-
-
     return i
 
-
-
-
-    # TODO probably you want to aggregate or combine these results here.
-    # For instance:
-    # results = [sum(result) for result in results]
-
-    # return the final results of the algorithm
-
-
-# TODO Feel free to add more central functions here.
